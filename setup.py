caa6946821399ec9a3445e6d8c35c3d812a9b7cf
Chris <57118805+chris-t-fernando@users.noreply.github.com>
2022-08-07 16:01:53 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="parameter_store",
+    version="0.1",
+    description="Interface for submitting and managing purchases across multiple crypto brokers",
+    author="Chris Fernando",
+    author_email="chris.t.fernando@gmail.com",
+    url="https://github.com/chris-t-fernando/parameter-store",
+    packages=["parameter_store"],
+    install_requires=["parameter_store"],
+)
