35b1204ab44d024e2702adc9e27c04029adaba9c
Chris <57118805+chris-t-fernando@users.noreply.github.com>
2022-08-07 20:29:44 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name="parameter_store",
     version="0.1",
-    description="Interface for submitting and managing purchases across multiple crypto brokers",
+    description="Interface and concretes for saving parameters for scripts",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
     url="https://github.com/chris-t-fernando/parameter-store",
     packages=["parameter_store"],
-    install_requires=["parameter_store"],
+    install_requires=["boto3"],
 )
