e512a788c5c96d2d9f067b7eeb585c8f2d641312
Chris <57118805+chris-t-fernando@users.noreply.github.com>
2022-11-19 13:37:46 +1100

    Fixed method signatures

--- parameter_store/iparameter_store.py
+++ parameter_store/iparameter_store.py
@@ -4,12 +4,14 @@ from abc import ABC, abstractmethod
 class IParameterStore(ABC):
     @abstractmethod
     def put(
+        self,
         Name: str,
         Value: str,
         Type: str = "String",
         Overwrite: bool = True,
     ) -> dict:
         ...
-
-    def get(Name: str, WithDecryption: bool = True) -> dict:
+        
+    @abstractmethod
+    def get(self, Name: str, WithDecryption: bool = True) -> dict:
         ...
